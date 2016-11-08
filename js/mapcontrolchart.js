23855cc67ebebedb90e363550a62ef535e95571f
Max von Hippel <maxvonhippel1996@gmail.com>
2016-11-08 11:44:11 +0545

    further improved mapcontrolchart.js performance

--- js/mapcontrolchart.js
+++ js/mapcontrolchart.js
@@ -1,29 +1,31 @@
-
+// initialize google charts and vars
 google.charts.load('current', {'packages':['annotationchart']});
 google.charts.setOnLoadCallback(init);
+var name = "data/newedit.csv";
+var div = "chart";
 
-function init() {
-	
-	jx("data/newedit.csv", "chart");
-	    		    
-}
-
+// called when range changes in chart
 function updateData(e) {
-
+	// showRange is a function in jsonclustermap.js
 	showRange(e['start'], e['end']);
 }
-	    
-function jx(name, div) {
-		    
+
+// used to initialize the map	    
+function init() {
+	// we asynchronously load the csv
+	// I use jQuery instead of httpxml lik in jsconclustermap because I want to use the csv function
+	// same performance basically, maybe a tiny bit slower    
 	jQuery.ajax({
-		
+		// initialize the ajax request
 		url: name,
 		async: true,
 		success: function(csvd) {
+			// on success wait until DOM is ready
 			$(document).ready(function(){
+				// initialize the DOM data array with csv parising
 				cdata = $.csv.toArrays(csvd, {onParseValue: $.csv.hooks.castToScalar});
 				for (var i = 1; i < cdata.length; i++) {
-							
+					// arrange by datestamp		
 					var parts = (cdata[i][0].split(" ")[0]).match(/(\d+)/g);
 					cdata[i][0] = new Date(parts[0], parts[1]-1, parts[2]);
 							
@@ -32,11 +34,10 @@ function jx(name, div) {
 				var chart = new google.visualization.AnnotationChart(document.getElementById(div));
 				google.visualization.events.addListener(chart, 'rangechange', updateData);
 				var options = {
-					displayAnnotations: true,
-					displayZoomButtons: false
-					// colors: ['#BBE876', '#A871E8', '#FF8F48']
+					displayAnnotations: false,
+					displayZoomButtons: false,
+					// colors: ['#BBE876', '#A871E8', '#FF8F48'] // uncomment to customize colors used in chart
 	    			};
-	
 				chart.draw(data, options);
 			});
 		},
