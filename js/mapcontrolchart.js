71ab9590ab7f54106aa2adbcb458ca3b0e8b4dc5
Max von Hippel <maxvonhippel1996@gmail.com>
2016-11-22 11:15:07 +0545

    clean up

--- js/mapcontrolchart.js
+++ js/mapcontrolchart.js
@@ -6,11 +6,24 @@ var div = "chart";
 var chart, data;
 var self = this;
 
-// used to initialize the map	    
+// inactivity listener for event end detection
+// len in milliseconds, func is a pointer to what should be done upon completion
+var itimer = function(len, func) {
+	this.len = len;
+	this.func = func;
+	this.timer = setTimeout(func, len);
+};
+
+itimer.prototype.interrupt = function() {
+	clearTimeout(this.timer);
+	this.timer = setTimeout(func, len);
+};
+
+// used to initialize the map
 function init() {
 	// we asynchronously load the csv
 	// I use jQuery instead of httpxml lik in jsconclustermap because I want to use the csv function
-	// same performance basically, maybe a tiny bit slower    
+	// same performance basically, maybe a tiny bit slower
 	jQuery.ajax({
 		// initialize the ajax request
 		url: name,
@@ -21,10 +34,10 @@ function init() {
 				// initialize the DOM data array with csv parising
 				cdata = $.csv.toArrays(csvd, {onParseValue: $.csv.hooks.castToScalar});
 				for (var i = 1; i < cdata.length; i++) {
-					// arrange by datestamp		
+					// arrange by datestamp
 					var parts = (cdata[i][0].split(" ")[0]).match(/(\d+)/g);
 					cdata[i][0] = new Date(parts[0], parts[1]-1, parts[2]);
-							
+
 				}
 				data = new google.visualization.arrayToDataTable(cdata);
 				chart = new google.visualization.AnnotationChart(document.getElementById(div));
@@ -35,38 +48,39 @@ function init() {
 					// colors: ['#BBE876', '#A871E8', '#FF8F48'] // uncomment to customize colors used in chart
 	    			};
 				chart.draw(data, options);
-				
-				//create trigger to resizeEnd event     
+
+				// create trigger to resizeEnd event
 				$(window).resize(function() {
 				    if(this.resizeTO) clearTimeout(this.resizeTO);
 				    this.resizeTO = setTimeout(function() {
 				        $(this).trigger('resizeEnd');
 				    }, 500);
 				});
-				
-				//redraw graph when window resize is completed  
+
+				//redraw graph when window resize is completed
 				$(window).on('resizeEnd', function() {
 				    chart.draw(data, options);
 				});
-				
+
 				// called on range change
 				function waitReady(e) {
 					// create a one-time listener for when the chart is ready
 					google.visualization.events.addOneTimeListener(chart, 'ready', function() {
+
 						//set the global variables [gStartTime] and [gEndTime] here:
 						gStartTime=e['start'];
 						gEndTime=e['end'];
 						//request data from the server with new [gStartTime] and [gEndTime]
 						request_for_data();
-						
+
 						// showRange is a function in jsonclustermap.js
 						self.showRange(e['start'], e['end']);
 						// update the selection statistics box
 						self.updateCardsRange(e['start'], e['end']);
-						
+
 					});
 				}
-				
+
 			});
 		},
 		dataType: "text",
