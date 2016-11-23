baacf700945fc933805bd6ac9f548d54f85c809d
share-hub <sazalsthapit@gmail.com>
2016-11-23 10:45:21 +0545

    Fixes issues encountered after merging

--- index.php
+++ index.php
@@ -52,201 +52,184 @@
 		</h1>
 	</div>
 
-		<div class="w3-row w3-center">
+	<div class="w3-row w3-center"> <!-- holds map, chart, nepal statistics and selected statistics -->
+		<div id="#mapchartbox" class="w3-border w3-border-blue-grey w3-col l9 s12 w3-display-container">
+			<div id="mapid" class="w3-border w3-col l9 s12">
+				<!-- For the searchbox -->
+				<div id="wrap" class="w3-col l4 s10 w3-display-topright">
+					<input id="searchBox" type="text" onfocus="this.select();" onkeyup="matched(this.value)" placeholder="Search for OSM username..." onfocusout="myfocusout()"/>
+					<!--<div id="console" style="visibility:hidden">
+					</div> -->
+					<ul id="username" class="w3-ul w3-card-2">
+					</ul>
+				</div> 
+				<!-- End of searchbox -->
+			</div> <!--- end of map -->
+			<script type="text/javascript" src="js/jsonclustermap.js"></script>
+			<!--- start of chart -->
+			<div id="chart" class="w3-border w3-col l9 s12"></div>
+			<script type="text/javascript" src="js/mapcontrolchart.js"></script>
+		</div> <!--- end of id=mapchartbox -->
+		
+		<div class="w3-col l3 s12 w3-light-grey w3-left">
+			<div id="nepalStatistics" class="w3-container">
+				<p class="w3-tooltip custom-tooltip">Nepal Statistics
 
-			<div id="#mapchartbox" class="w3-border w3-border-blue-grey w3-col l9 s12 w3-display-container">
-				<div id="mapid" class="w3-border w3-col l9 s12">
-					<!-- For the searchbox -->
-					<div id="wrap" class="w3-col l4 s10 w3-display-topright">
-						<input id="searchBox" type="text" onfocus="this.select();" onkeyup="matched(this.value)" placeholder="Search for OSM username..." onfocusout="myfocusout()"/>
-						<!--<div id="console" style="visibility:hidden">
-						</div> -->
-						<ul id="username" class="w3-ul w3-card-2">
-						</ul>
-					</div>
-					<!-- End of searchbox -->
-				</div>
-				<script type="text/javascript" src="js/jsonclustermap.js"></script>
-				<!--- start of chart -->
-				<div id="chart" class="w3-border w3-col l9 s12"></div>
-				<script type="text/javascript" src="js/mapcontrolchart.js"></script>
+					<img src="resources/i_icon.png" width="20px"</img>
+					<span class="w3-text w3-card-2 w3-animate-opacity w3-light-grey tooltip">
+						Shows the cumulative statistics of the entire Nepal
+					</span>
+				</p>
 
+				<table id="tblNepalStatistics" class="w3-table-all w3-card-2" >
+					<tr>
+						<th class="th_left">Features</th>
+						<th class="th_right">Count</th>
+					</tr>
+					<tr>
+						<td>Mappers</td>
+						<td id="nepalMappers" class="right">1,920</td>
 
-				<!--- end of chart -->
+					</tr>
+					<tr>
+						<td>Buildings</td>
+
+						<td id="nepalBuildings" class="right">600,000</td>
+					</tr>
+					<tr>
+						<td>Roads (in km)</td>
+						<td id="nepalRoads" class="right">94,000</td>
+					</tr>
+					<tr>
+						<td>Schools</td>
+						<td id="nepalSchools" class="right">3,216</td>
+					</tr>
+					<tr>
+						<td>Hospitals</td>
+						<td id="nepalHospitals" class="right">1,012</td>
+					</tr>
+				</table>
 			</div>
-			<!--- end of map -->
-
-
-			<div class="w3-col l3 s12 w3-light-grey w3-left">
-				<div id="nepalStatistics" class="w3-container">
-					<p class="w3-tooltip custom-tooltip" style="margin-top: 30px;">Nepal Statistics
-
-
-				<!--- end of chart --->
-			</div>
-			<!--- end of map --->
-
-
-			<div class="w3-col l3 s12 w3-light-grey w3-left">
-				<div id="nepalStatistics" class="w3-container">
-					<p class="w3-tooltip custom-tooltip">Nepal Statistics
-
-						<img src="resources/i_icon.png" width="20px"</img>
-						<span class="w3-text w3-card-2 w3-animate-opacity w3-light-grey tooltip">
-							Shows the cumulative statistics of the entire Nepal
-						</span>
-					</p>
-
-					<table id="tblNepalStatistics" class="w3-table-all w3-card-2" >
-						<tr>
-							<th class="th_left">Features</th>
-							<th class="th_right">Count</th>
-						</tr>
-						<tr>
-							<td>Mappers</td>
-							<td id="nepalMappers" class="right">1,920</td>
-
-						</tr>
-						<tr>
-							<td>Buildings</td>
-
-							<td id="nepalBuildings" class="right">600,000</td>
-						</tr>
-						<tr>
-							<td>Roads (in km)</td>
-							<td id="nepalRoads" class="right">94,000</td>
-						</tr>
-						<tr>
-							<td>Schools</td>
-							<td id="nepalSchools" class="right">3,216</td>
-						</tr>
-						<tr>
-							<td>Hospitals</td>
-							<td id="nepalHospitals" class="right">1,012</td>
-						</tr>
-					</table>
-				</div>
-				<hr class="style-one"/>
-				<div id="selectionStatistics" class="w3-container">
-					<p class="w3-tooltip custom-tooltip">Selection Statistics
-						<img src="resources/i_icon.png" width="20px"</img>
-						<span class="w3-text w3-card-2 w3-animate-opacity w3-light-grey tooltip">
-							Shows the statisics of the selected user within the selected date range and the selected (zoomed in) area of the map
-						</span>
-					</p>
-					
-					<table class="w3-table-all w3-card-2" align="center">
-						<tr>
-							<th class="th_left">Features</th>
-							<th id="startDate" class="th_center">18 March 2007</th>
-							<th id="endDate" class="th_right">Oct 2016</th>
-						</tr>
-						<tr>
-							<td>Buildings</td>
-							<td id="selectedBuildings_start" class="center">2,114</td>
-							<td id="selectedBuildings_end" class="right"class="right">600,000</td>
-						</tr>
-						<tr>
-							<td>Roads (in km)</td>
-							<td id="selectedRoads_start" class="center">1,517</td>
-							<td id="selectedRoads_end" class="right">94,000</td>
-						</tr>
-						<tr>
-							<td>Schools</td>
-							<td id="selectedSchools_start" class="center">53</td>
-							<td id="selectedSchools_end" class="right">3,216</td>
-						</tr>
-						<tr>
-							<td>Hospitals</td>
-							<td id="selectedHospitals_start" class="center">23</td>
-							<td id="selectedHospitals_end" class="right">1,012</td>
-						</tr>
-					</table>
-					<p align="right" style="font-style:italic;font-family:testFont">Currently showing statistics for username: Sazal(Solaris)<br/>within 18 July 2016 and 21 Nov 2016</p>
-				</div>
+			<hr class="style-one"/>
+			<div id="selectionStatistics" class="w3-container">
+				<p class="w3-tooltip custom-tooltip">Selection Statistics
+					<img src="resources/i_icon.png" width="20px"</img>
+					<span class="w3-text w3-card-2 w3-animate-opacity w3-light-grey tooltip">
+						Shows the statisics of the selected user within the selected date range and the selected (zoomed in) area of the map
+					</span>
+				</p>
+				
+				<table class="w3-table-all w3-card-2" align="center">
+					<tr>
+						<th class="th_left">Features</th>
+						<th id="startDate" class="th_center">18 March 2007</th>
+						<th id="endDate" class="th_right">Oct 2016</th>
+					</tr>
+					<tr>
+						<td>Buildings</td>
+						<td id="selectedBuildings_start" class="center">2,114</td>
+						<td id="selectedBuildings_end" class="right"class="right">600,000</td>
+					</tr>
+					<tr>
+						<td>Roads (in km)</td>
+						<td id="selectedRoads_start" class="center">1,517</td>
+						<td id="selectedRoads_end" class="right">94,000</td>
+					</tr>
+					<tr>
+						<td>Schools</td>
+						<td id="selectedSchools_start" class="center">53</td>
+						<td id="selectedSchools_end" class="right">3,216</td>
+					</tr>
+					<tr>
+						<td>Hospitals</td>
+						<td id="selectedHospitals_start" class="center">23</td>
+						<td id="selectedHospitals_end" class="right">1,012</td>
+					</tr>
+				</table>
+				<p align="right" style="font-style:italic;font-family:testFont">Currently showing statistics for username: Sazal(Solaris)<br/>within 18 July 2016 and 21 Nov 2016</p>
 			</div>
 		</div>
+	</div> <!-- END OF holds map, chart, nepal statistics and selected statistics -->
 
 
 
-		<h2 id="sectionTitle" class="sectionTitle">
+	<h2 id="sectionTitle" class="sectionTitle">
 		<span id="sectionOpenStreetMap">OpenStreetMap Nepal</span>
 		<span id="sectionLeaderboards">LeaderBoards</span>
-		</h2>
+	</h2>
+	<div class="w3-container w3-row w3-center"> <!-- Holds all leaderboard tables -->
+		<div class="custom-tooltip w3-col l6 s12 w3-display-container"><p>Nodes</p>
+			<table id="tblNodes" class="w3-table-all w3-card-2 w3-col l11 s12 center"><!--Nodes Table-->
+				<thead>
+					<tr>
+						<th class="th_center">Rank</th>
+						<th class="th_center">OSM Username</th>
+						<th class="th_center">Nodes</th>
+						<th class="th_right">Most Frequently edited POI</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="w3-blue highlighted">
+						<td class="center">1</td>
+						<td class="center">Nama Budhathoki</td>
+						<td class="center">12,14,145</td>
+						<td class="center">Restaurant</td>
 
-
-		<h2 class="sectionTitle">OpenStreetMap Nepal Leaderboards</h2>
-		<div class="w3-container w3-row w3-center">
-			<div class="custom-tooltip w3-col l6 s12 w3-display-container"><p>Nodes</p>
-				<table id="tblNodes" class="w3-table-all w3-card-2 w3-col l11 s12 center"><!--Nodes Table-->
-					<thead>
-						<tr>
-							<th class="th_center">Rank</th>
-							<th class="th_center">OSM Username</th>
-							<th class="th_center">Nodes</th>
-							<th class="th_right">Most Frequently edited POI</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr class="w3-blue highlighted">
-							<td class="center">1</td>
-							<td class="center">Nama Budhathoki</td>
-							<td class="center">12,14,145</td>
-							<td class="center">Restaurant</td>
-
-						</tr>
-						<tr>
-							<td class="center">2</td>
-							<td class="center">Pratik Gautam</td>
-							<td class="center">10,18,216</td>
-							<td class="center">Airport</td>
-						</tr>
-						<tr>
-							<td class="center">3</td>
-							<td class="center">Sazal(Solaris)</td>
-							<td class="center">10,18,216</td>
-							<td class="center">Airport</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			<div class="custom-tooltip w3-col l6 s12 w3-display-container"><p>Ways</p>
-				<table id= "tblWays" class="w3-table-all w3-card-2 w3-col l11 s12 center"><!--Ways Table-->
-					<thead>
-						<tr>
-							<th td class="center">Rank</th>
-							<th td class="center">OSM Username</th>
-							<th td class="center">Ways</th>
-							<th td class="right">Most Frequently edited POI</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr class="w3-deep-orange highlighted">
-							<td class="center">1</td>
-							<td class="center">Sazal(Solaris)</td>
-							<td class="center">10,18,216</td>
-							<td class="center">Airport</td>
-						</tr>
-						<tr>
-							<td class="center">2</td>
-							<td class="center">Nama Budhathoki</td>
-							<td class="center">12,14,145</td>
-							<td class="center">Restaurant</td>
-						</tr>
-						<tr>
-							<td class="center">3</td>
-							<td class="center">Pratik Gautam</td>
-							<td class="center">10,18,216</td>
-							<td class="center">Airport</td>
-
-						</tr>
-					</tbody>
-				</table>
-			</div>
-
+					</tr>
+					<tr>
+						<td class="center">2</td>
+						<td class="center">Pratik Gautam</td>
+						<td class="center">10,18,216</td>
+						<td class="center">Airport</td>
+					</tr>
+					<tr>
+						<td class="center">3</td>
+						<td class="center">Sazal(Solaris)</td>
+						<td class="center">10,18,216</td>
+						<td class="center">Airport</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
-		<br/><br/>
-		<!-- </div> <!-- id "glassy-effect" -->
-	</body>
+		<div class="custom-tooltip w3-col l6 s12 w3-display-container"><p>Ways</p>
+			<table id= "tblWays" class="w3-table-all w3-card-2 w3-col l11 s12 center"><!--Ways Table-->
+				<thead>
+					<tr>
+						<th td class="center">Rank</th>
+						<th td class="center">OSM Username</th>
+						<th td class="center">Ways</th>
+						<th td class="right">Most Frequently edited POI</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="w3-deep-orange highlighted">
+						<td class="center">1</td>
+						<td class="center">Sazal(Solaris)</td>
+						<td class="center">10,18,216</td>
+						<td class="center">Airport</td>
+					</tr>
+					<tr>
+						<td class="center">2</td>
+						<td class="center">Nama Budhathoki</td>
+						<td class="center">12,14,145</td>
+						<td class="center">Restaurant</td>
+					</tr>
+					<tr>
+						<td class="center">3</td>
+						<td class="center">Pratik Gautam</td>
+						<td class="center">10,18,216</td>
+						<td class="center">Airport</td>
+
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div> <!-- END OF Holds all leaderboard tables -->
+	
+	<br/>
+	<br/>
+	
+	<!-- </div> <!-- id "glassy-effect" -->
+</body>
 </html>
 
