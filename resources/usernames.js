6162e5d15d886fb5cd00c3f033c7d8829175c70e
Max von Hippel <maxvonhippel1996@gmail.com>
2016-11-23 16:34:18 +0545

    saving small progress with usernames

--- resources/usernames.js
+++ resources/usernames.js
@@ -2,3 +2,12 @@ usernames=[
 "Sazal(Solaris)",
 "NamaBudhathoki",
 "PratikGautam"];
+
+if (GLOBALDEBUG == FALSE) {
+	// try to connect to server
+	$.get( "http://192.168.1.25:8000/usernames/")
+	.done(function( data ) {
+    	usernames = data;
+    	console.log(data);
+  	});
+}
