8780c0e3ec6dee52583d25d2cddef39f84abe9a2
Max von Hippel <maxvonhippel1996@gmail.com>
2016-12-07 15:35:34 +0545

    CORS GALORE

--- js/getdata.js
+++ js/getdata.js
@@ -1,8 +1,6 @@
 // this script gets data from the server
 
-
-var baseurl = "http://127.0.0.1:8080/";
-
+var baseurl = "http://139.59.37.112:8080/";
 
 // Create the XHR object.
 function createCORSRequest(method, url) {
@@ -50,48 +48,17 @@ function makeCorsRequest(url) {
   	xhr.send();
 }
 
-
-
-// get and return the array of usernames
-// example: see server/geodjango/example output/testusernames.txt
 function usernames () {
 	console.log("requesting usernames");
 	// format the url
-	url = baseurl + "usernames/";
+	var url = baseurl + "usernames/";
 
 	var usernames = [];
-	$.ajax({
-
-    	url: url,
-		data: null,
-		type: 'GET',
-    	crossDomain: true,
-		dataType: 'txt',
-		success: function(data) {
-			usernames = data;
-			console.log("got usernames:");
-    		console.log(usernames);
-    		return usernames;
-		},
-		error: function(xhr, status, text) {
-	        var response = xhr.responseText;
-
-	        console.log('Failure!');
-
-	        if (response)
-			console.log(response.error);
-		}
-
-	});
-	return null;
-
-	return makeCorsRequest(url);
+	usernames = makeCorsRequest(url);
+	return usernames;
 
 }
 
-// get and return the json of country data
-// example:
-// {"Building": 22560, "Roads": 2512, "Education": 1, "Health": 9, "Mappers": 9465}
 function country_stats () {
 	/*Remove from here.... when CORS is taken care of*/
 		/*var cStats={"Buildings": 22560, "Roads": 2512, "Education": 1, "Health": 9, "Mappers": 9465};
@@ -100,16 +67,10 @@ function country_stats () {
 	/*Remove upto here.... when CORS is taken care of*/
 
 	// format the url
-	url = baseurl + "jsoncountry/";
-	// get the data
-	$.get(url)
-	.done(function(cStats) {
-		//return data;
-		//var cStats={"Buildings": 22560, "Roads": 2512, "Education": 1, "Health": 9, "Mappers": 9465};
-		updateNepalStatistics(cStats);
-		return cStats; // doesn't do much; just returning in keeping up with the norm.
-	});
-	return null;
+	var url = baseurl + "jsoncountry/";
+	var cStats = makeCorsRequest(url);
+	updateNepalStatistics(cStats);
+	return cStats; // doesn't do much; just returning in keeping up with the norm.
 }
 
 // get and return the json of selection data
@@ -132,36 +93,11 @@ function selection_stats (mn_x, mn_y, mx_x, mx_y, start, end, user) {
 	// format the url
 	if (!user || user == null || user == "")
 		user = "";
-	url = baseurl + "jsonselection/" + start.getFullYear() + "-" + start.getMonth() + "-" + start.getDay() + "," + end.getFullYear() + "-" + end.getMonth() + "-" + end.getDay() + "/" + mn_x.toString() + "/" + mn_y.toString() + "/" + mx_x.toString() + "/" + mx_y.toString() + "/" + user + "/";
+	var url = baseurl + "jsonselection/" + start.getFullYear() + "-" + start.getMonth() + "-" + start.getDay() + "," + end.getFullYear() + "-" + end.getMonth() + "-" + end.getDay() + "/" + mn_x.toString() + "/" + mn_y.toString() + "/" + mx_x.toString() + "/" + mx_y.toString() + "/" + user + "/";
 
-	debugger;
-
-	$.ajax({
-	    url: url,
-		type: "GET",
-	    dataType: "json",
-	    timeout: 600000,	// Set your timeout value in milliseconds or 0 for unlimited
-	    success: function(response) {
-
-			debugger;
-			updateSelectionStatistics(response);
-			return response;
-		},
-	    error: function(jqXHR, textStatus, errorThrown) {
-	        if(textStatus==="timeout") {
-	            console.log("Call has timed out");	// Handle the timeout
-			} else {
-	            console.log("Another error was returned");	// Handle other error type
-			}
-		}
-	});
-
-	console.log("nothing found on server for selection");
-	return null;
-	//return 0;
-	/*Remove upto here.... when CORS is taken care of*/
-
-
+	var response = makeCorsRequest(url);
+	updateSelectionStatistics(response);
+	return response;
 }
 
 
@@ -171,48 +107,26 @@ function nodes_stats(ret, start, end){
 	var nums = [ "first", "second", "third", "fourth", "fifth" ];
 	// ^topnodes/timerange/mn_x/mn_y/mx_x/mx_y/first/second/third/fourth/fifth/
 	var url = baseurl + "topnodes/" + start.getFullYear() + "-" + start.getMonth() + "-" + start.getDay() + "," + end.getFullYear() + "-" + end.getMonth() + "-" + end.getDay() + "/" + mn_x.toString() + "/" + mn_y.toString() + "/" + mx_x.toString() + "/" + mx_y.toString() + "/" + ret[0] + "/" + ret[1] + "/" + ret[2] + "/" + ret[3] + "/" + ret[4] + "/";
-	/*
-	for (var key in ret) {
-		console.log("Place: ", ++place, " User: ", key, " Nodes mapped: ", ret[key]);
-	}*/
-	debugger;
 
-	$.ajax({
-	    url: url,
-		type: "GET",
-	    dataType: "json",
-	    timeout: 600000,	// Set your timeout value in milliseconds or 0 for unlimited
-	    success: function(response) {
-
-			debugger;
-			// {"\u9ec3\u9756\u5a9b": "none", "samely": "crossing",`"048296609": "none", "12marina9": "none", "2370save": "none"}`
-			var st = 0;
-			Papa.parse(response, {
-				download: true, 		// downloads the file, otherwise it doesn't work
-				dynamicTyping: true, 	// automatically figures out if something is a string, number, etc
-				delimiter: ",", 		// explicit statement improves speed
-				worker: true,
-				step: function(row) {
-					// row.data[0]
-					var arr = row.data[0].split(':');
-					nodesTable[nums[st]] = {};
-					nodesTable[nums[st]]["Rank"] = st + 1;
-					nodesTable[nums[st]]["OSM Username"] = arr[0];
-					nodesTable[nums[st]]["Most Frequently edited POI"] = arr[1];
-					++st;
-				},
-				complete: function() {
-					updateNodes(nodesTable);
-					return 0;
-				}
-			});
+	var response = makeCorsRequest(url);
+	var st = 0;
+	Papa.parse(response, {
+		download: true, 		// might not need this
+		dynamicTyping: true, 	// automatically figures out if something is a string, number, etc
+		delimiter: ",", 		// explicit statement improves speed
+		worker: true,
+		step: function(row) {
+			// row.data[0]
+			var arr = row.data[0].split(':');
+			nodesTable[nums[st]] = {};
+			nodesTable[nums[st]]["Rank"] = st + 1;
+			nodesTable[nums[st]]["OSM Username"] = arr[0];
+			nodesTable[nums[st]]["Most Frequently edited POI"] = arr[1];
+			++st;
 		},
-	    error: function(jqXHR, textStatus, errorThrown) {
-	        if(textStatus==="timeout") {
-	            console.log("Call has timed out");	// Handle the timeout
-			} else {
-	            console.log("Another error was returned");	// Handle other error type
-			}
+		complete: function() {
+			updateNodes(nodesTable);
+			return 0;
 		}
 	});
 	return 1; // failure
